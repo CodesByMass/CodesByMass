4f438ea54747f304d1e2b7148cc61ac2cfb00e21
Massyl Selmi <mass.selmi@gmail.com>
2020-03-30 08:01:30 +0200

    Add Operation.java

---
+++ src/main/java/Operation.java
@@ -0,0 +1,54 @@
+public enum Operation {
+
+  // Double bien plus précis qu'un float
+  PLUS('+') {
+    @Override
+    public double eval(double operand_a, double operand_b) {
+      return operand_a + operand_b;
+    }
+  },
+  MOINS('-') {
+
+    @Override
+    public double eval(double operand_a, double operand_b) {
+      return operand_a - operand_b;
+    }
+  },
+  MULT('*') {
+    @Override
+    public double eval(double operand_a, double operand_b) {
+      return operand_a * operand_b;
+    }
+  },
+  DIV('/') {
+    @Override
+    public double eval(double operand_a, double operand_b) throws ArithmeticException {
+      if (operand_b == 0) {
+        throw new ArithmeticException("Division par 0 impossible !");
+      } else
+        return operand_a / operand_b;
+    }
+  },
+  MOD('%') {
+    @Override
+    public double eval(double operand_a, double operand_b) {
+      if (operand_b == 0) {
+        throw new ArithmeticException("Division par 0 impossible !");
+      }
+      return operand_a % operand_b;
+    }
+  };
+
+  private char symbole;
+
+  Operation(char symbole) {
+    this.symbole = symbole;
+  }
+
+  public abstract double eval(double operand_1, double operand_2);
+
+  public char getOperation() {
+    return symbole;
+  }
+
+}
